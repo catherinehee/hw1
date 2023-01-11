884e308bfbd44e1a4c7347e4573b292ed0959212
Catherine He <hecy@usc.edu>
2023-01-11 23:41:27 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -9,15 +9,34 @@ To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
-
+#include <cstddef>
 #include "split.h"
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in == NULL)
+  {
+    if (odds != NULL)
+      odds = NULL;
+    if (evens != NULL)
+      evens = NULL;
+    return;
+  }
+  if (in -> value % 2 == 0)
+  {
+    evens = in;
+    in = in ->next;
+    return split(in, odds, evens->next);
+  }
+  else
+  {
+    odds = in;
+    in = in ->next;
+    return split(in, odds->next, evens);
+  }
 }
 
 /* If you needed a helper function, write it here */
